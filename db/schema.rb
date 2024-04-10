444617809a5f647a10864d1ef4eb4bdd3b057f53
Anastasiia Kovalenko <n.kovalenko@sloboda-studio.com>
2024-04-10 17:09:26 +0200

    Added Author model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_10_150716) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_10_150845) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -47,6 +47,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_10_150716) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "authors", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authors_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -95,6 +102,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_10_150716) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "authors", "users"
   add_foreign_key "recipe_tags", "recipes"
   add_foreign_key "recipe_tags", "tags"
   add_foreign_key "recipes", "categories"
